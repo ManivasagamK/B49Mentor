bf4bb50dcfbdb7b6b0adb04e44e270ba58efbcf4
mani <manivasagam55555@gmail.com>
2023-06-28 11:47:39 +0530

    add

---
+++ Day 4 Assignment/Anonymous and IIFE/Odd number.js
@@ -0,0 +1,20 @@
+//Anonymous:
+let Array=[1,2,3,4];
+for(let i=0; i<Array.length;i++){
+let odd = function(value){
+    if(value % 2 !== 0){
+        console.log(value);
+    }
+    }
+    odd(Array[i]);
+}
+
+//IIFE
+let Array1=[1,2,3,4];
+for(let i=0; i<Array1.length;i++){
+    if(Array1[i] % 2 !== 0){
+        (function(){
+        console.log(Array1[i]);
+        })()
+    }
+};
