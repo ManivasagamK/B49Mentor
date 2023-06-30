be99ffd467353b3cdb6014d3a847bc373ab5b448
mani <manivasagam55555@gmail.com>
2023-06-30 11:51:31 +0530

    Add

---
+++ Day 5 Assignment/JSONresume.js
@@ -0,0 +1,61 @@
+var JSONResume=`{
+  "BasicDetails": [
+    {
+    "name": "Manivasagam K",
+    "label": "Mr.",
+    "email": "manivasagam55555@gmail.com",
+    "phone": "9688928441",
+    "degree": "M.Sc.Agri",
+    "location": {
+      "address": "Ganapathipalayam udumalpet tirupur",
+      "postalCode": "642122",
+      "city": "Udumalpet",
+      "countryCode": "India",
+      "region": "Tamil Nadu"
+    },
+  }
+],
+  "Education": [
+    {
+      "Institution": "TAMIL NADU AGRICULTURAL UNIVERSITY",
+      "AreaOfStudy": "Agriculture",
+      "studyType": "Fulltime",
+      "Startyear": "2019",
+      "Endyear": "2021",
+      "gpa": "8.79/10"
+    }
+  ],
+  "Awards": [
+    {
+      "Title": "Zonal Ballbadmiton championship",
+      "AwarderBy": "TamilNadu Sport Academy"
+    }
+  ],
+  "Publications": [
+    {
+      "Name": "Ecofriendly Management of coconut rhinoceros beetle using Metarhizium and Botanicals",
+      "Publisher": "Ecology, Environment and Conservation",
+      "PublishedYear": "jan-2022"
+    }
+  ],
+  "Skills": [
+    {
+      "Name": "MS Office,Farming, Agricultural Analysis, Landscaping "
+      "level": "Intermediate",
+      ]
+    }
+  ],
+  "Languages": [
+    {
+      "language": "Tamil and English",
+      "fluency": "Professional"
+    }
+  ],
+  "Interests": [
+    {
+      "Name": {"Cricket", 'Batmiton', 'Travel'},
+    }
+  ]
+}`;
+
+console.log(JSONResume);
