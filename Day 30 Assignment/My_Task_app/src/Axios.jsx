33142afb05cea3736dfe4fda80ee1be8121c8b4e
mani <manivasagam55555@gmail.com>
2023-08-26 13:56:43 +0530

    Add

--- Day 30 Assignment/My_Task_app/src/Axios.jsx
+++ Day 30 Assignment/My_Task_app/src/Axios.jsx
@@ -10,11 +10,54 @@ function Axios() {
         setPost(data.data);
       });
     }, []);
-  
+  // console.log(post)
+  const handleEdit=(item,index)=>{
+
+  }
     return (
         <>
         <h1>Axios</h1>
-      <div>
+        <div className='form'>
+        <div className="input">
+        <label htmlFor='name'>Name:</label>
+        <input type='text' id='name'/>
+        </div>
+        <br/>
+        {/* <label htmlFor='name'>Name:</label>
+        <input type='text' id='name'/> */}
+        <div className="input">
+        <label htmlFor='username'>UserName:</label>
+        <input type='text' id='username'/>
+        </div>
+        <br/>
+        <div className="input">
+        <label htmlFor='companyname'>CompanyName:</label>
+        <input type='text' id='companyname'/>
+        </div>
+        <br/>
+        <div className="input">
+        <label htmlFor='specialisation'>Specialisation:</label>
+        <input type='text' id='specialisation'/>
+        </div>
+        <br/>
+        <div className="input">
+        <label htmlFor='cityname'>CityName:</label>
+        <input type='text' id='cityname'/>
+        </div>
+        <br/>
+        <div className="input">
+        <label htmlFor='phone'>PhoneNo:</label>
+        <input type='text' id='phone'/>
+        </div>
+        <br/>
+        <div className="input">
+        <label htmlFor='email'>Email-Id:</label>
+        <input type='text' id='email'/>
+        </div>
+        <br/>
+        <button type='submit' >Add</button>
+        </div>
+      <div className='main'>
         
         {post.map((item, i) => {
             return(
@@ -27,7 +70,8 @@ function Axios() {
               <p><strong>CityName:</strong>{` `+item.address.city}</p>
               <p><strong>PhoneNo:</strong>{` `+ item.phone}</p>
               <p><strong>Email-Id:</strong>{` `+ item.email}</p>
-              
+              <button className='btn btn-info' onClick={handleEdit}><strong>Edit</strong></button>
+              <button className='btn btn-danger' onClick={handleDelete}><strong>Delete</strong></button>
             
               </div>
         </div>
