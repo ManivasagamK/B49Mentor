f8c9b2c9ecf625b931d8312cd66d9d2fc43daf6e
mani <manivasagam55555@gmail.com>
2023-08-28 10:16:53 +0530

    add

--- Day 30 Assignment/My_Task_app/src/Axios.jsx
+++ Day 30 Assignment/My_Task_app/src/Axios.jsx
@@ -3,16 +3,29 @@ import axios from 'axios'
 import './Axios.css'
 function Axios() {
     const [post, setPost] = useState([]);
-
+    console.log(post);
+    
     useEffect(() => {
       axios.get("https://jsonplaceholder.typicode.com/users").then((data) => {
         console.log(data);
         setPost(data.data);
       });
     }, []);
+    
   // console.log(post)
+  const handleAdd=(event)=>{
+    event.preventDefault();
+    let nameInput = document.getElementById("name");
+    let userInput = document.getElementById("username");
+    let companyInput = document.getElementById("companyname");
+    let specInput = document.getElementById("specialisation");
+
+  }
   const handleEdit=(item,index)=>{
 
+  }
+  const handleDelete=(item,i)=>{
+    post.splice(i,1);
   }
     return (
         <>
@@ -55,7 +68,7 @@ function Axios() {
         <input type='text' id='email'/>
         </div>
         <br/>
-        <button type='submit' >Add</button>
+        <button type='submit' onSubmit={handleAdd}>Add</button>
         </div>
       <div className='main'>
         
@@ -65,8 +78,8 @@ function Axios() {
             <div className='card'>
               <p><strong>Name:</strong>{` `+item.name}</p>
               <p><strong>UserName:</strong>{` `+ item.username}</p>
-              <p><strong>CompanyName:</strong>{` `+ item.company.name}</p>
               <p><strong>Specialisation:</strong>{` `+item.company.catchPhrase}</p>
+              <p><strong>CompanyName:</strong>{` `+ item.company.name}</p>
               <p><strong>CityName:</strong>{` `+item.address.city}</p>
               <p><strong>PhoneNo:</strong>{` `+ item.phone}</p>
               <p><strong>Email-Id:</strong>{` `+ item.email}</p>
