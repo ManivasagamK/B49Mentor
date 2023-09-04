eb473614f9f8f5c326f735f4264814987e33e6a0
mani <manivasagam55555@gmail.com>
2023-09-04 12:03:54 +0530

    Add

--- Day 30 Assignment/My_Task_app/src/App.jsx
+++ Day 30 Assignment/My_Task_app/src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Axios from './Axios'
-import Axios1 from './Axios1'
-//import Crud from './Crud'
 
 function App() {
  
@@ -12,7 +7,6 @@ function App() {
   return (
     <>
       <Axios/>
-      {/* <Axios1/> */}
       
     </>
   )
