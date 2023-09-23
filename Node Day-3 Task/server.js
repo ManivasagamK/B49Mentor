d014e7730403434d0481610c983ce3be1cca7a97
mani <manivasagam55555@gmail.com>
2023-09-23 08:01:06 +0530

    Add

--- Node Day-3 Task/server.js
+++ Node Day-3 Task/server.js
@@ -9,93 +9,98 @@ const Student = require("./Models/Student");
 
 const mani = express();
 
-const PORT = process.env.PORT;
-const DB_URL= process.env.DB_URL;
+const PORT = 5000;
+const DB_URL =
+  "mongodb+srv://manivasagam:8778421906Mk@mentorandstudent.itu531b.mongodb.net/";
 
 mani.use(bodyparser.json());
-
 
 mongoose
   .connect(DB_URL, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB", err));
-mani.post("/mentor",async (req,res)=>{
-    try {
-        const mentor =new Mentor(req.body);
-        await mentor.save();
-        res.send(mentor);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+mani.post("/mentor", async (req, res) => {
+  try {
+    const mentor = new Mentor(req.body);
+    await mentor.save();
+    res.send(mentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-mani.post("/student",async (req,res)=>{
-    try {
-        const student =new Student(req.body);
-        await student.save();
-        res.send(student);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+mani.post("/student", async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    await student.save();
+    res.send(student);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 mani.post("/mentor/:mentorId/assign", async (req, res) => {
-    try {
-      const mentor = await Mentor.findById(req.params.mentorId);
-      const students = await Student.find({ _id: { $in: req.body.students } });
-      students.forEach((student) => {
-        student.cMentor = mentor._id;
-        student.save();
-      });
-      mentor.students = [
-        ...mentor.students,
-        ...students.map((student) => student._id),
-      ];
-      await mentor.save();
-      res.send(mentor);
-    } catch (error) {
-      res.status(400).send(error);
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId);
+    const students = await Student.find({ _id: { $in: req.body.students } });
+    students.forEach((student) => {
+      student.cMentor = mentor._id;
+      student.save();
+    });
+    mentor.students = [
+      ...mentor.students,
+      ...students.map((student) => student._id),
+    ];
+    await mentor.save();
+    res.send(mentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+mani.put("/student/:studentId/assignMentor/:mentorId", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.studentId);
+    const nMentor = await Mentor.findById(req.params.mentorId);
+
+    if (student.cMentor) {
+      const Mentor = await Mentor.findById(student.cMentor);
+      Mentor.students.splice(indexOf(student), 1);
+      student.pMentor.push(student.cMentor);
     }
-  });
-  
-  mani.put("/student/:studentId/assignMentor/:mentorId", async (req, res) => {
-    try {
-      const student = await Student.findById(req.params.studentId);
-      const nMentor = await Mentor.findById(req.params.mentorId);
-  
-      if (student.cMentor) {
-        student.pMentor.push(student.cMentor);
-      }
-  
+    if (student.pMentor.includes(nMentor._id)) {
+      res.status(402).send("current Mentor and previous mentor cannot be same");
+    } else {
       student.cMentor = nMentor._id;
       await student.save();
       res.send(student);
-    } catch (error) {
-      res.status(400).send(error);
     }
-  });
-  
-  mani.get("/mentor/:mentorId/students", async (req, res) => {
-    try {
-      const mentor = await Mentor.findById(req.params.mentorId).populate(
-        "students"
-      );
-      res.send(mentor.students);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
-  mani.get("/student/:studentId/pMentor", async (req, res) => {
-    try {
-      const student = await Student.findById(req.params.studentId).populate(
-        "pMentor"
-      );
-      res.send(student.pMentor);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-mani.listen(PORT,()=>{
-    console.log(`server running in the port- ${PORT}`);
-})
+mani.get("/mentor/:mentorId/students", async (req, res) => {
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId).populate(
+      "students"
+    );
+    res.send(mentor.students);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+mani.get("/student/:studentId/pMentor", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.studentId).populate(
+      "pMentor"
+    );
+    res.send(student.pMentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+mani.listen(PORT, () => {
+  console.log(`server running in the port- ${PORT}`);
+});
