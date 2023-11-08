239f5d65472c96fcdf168d6bf4ef9ed53b86d099
ManivasagamK <manivasagam55555@gmail.com>
2023-11-08 20:21:06 +0530

    Add

--- Node Day-6 - Url shortner/Front End/Url-Shortner/src/Components/Signup.jsx
+++ Node Day-6 - Url shortner/Front End/Url-Shortner/src/Components/Signup.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
@@ -96,103 +96,5 @@ const Signup = () => {
   );
 };
 
-=======
-import React, { useState } from "react";
-import { Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const { name, email, password } = formData;
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(formData);
-    try {
-      const response = await fetch(
-        "https://urlshortnerapi.onrender.com/api/users/signup",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-
-      if (response) {
-        console.log("Signup successful!");
-        navigate("/");
-      } else {
-        console.error("Signup failed.");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <Container>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Signup
-        </button>
-      </form>
-    </Container>
-  );
-};
-
->>>>>>> 7f069b90857f9c3f6af5767f193cea747780b0c4
 export default Signup;
